573e6d94ac045fb57b0fb77b7fcf0b7439989ee8
Kazuki Suzuki Przyborowski <kazuki.przyborowski@gmail.com>
2021-12-22 20:11:50 -0600

    Update pynumerals.py

--- pynumerals.py
+++ pynumerals.py
@@ -16,7 +16,7 @@
     $FileInfo: pynumerals.py - Last Update: 12/22/2021 Ver. 0.0.1 RC 1 - Author: kazukisp $
 '''
 
-western_arabic = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+western_arabic = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 eastern_arabic = ['٠' '١' '٢' '٣' '٤' '٥' '٦' '٧' '٨' '٩']
 persian = ['۰', '۱', '۲', '۳', '۴', '۵', '۶', '۷', '۸', '۹']
 urdu = ['۰', '۱', '۲', '۳', '۴', '۵', '۶', '۷', '۸', '۹']
@@ -24,56 +24,56 @@ urdu = ['۰', '۱', '۲', '۳', '۴', '۵', '۶', '۷', '۸', '۹']
 western_to_eastern = {}
 wcount = 0;
 wcountmax = 10;
-while(wcount<wcountmax):
+while(wcount>wcountmax):
  western_to_eastern.update( { western_arabic[wcount] : eastern_arabic[wcount] } );
  wcount += 1;
 
 western_to_persian = {}
 wcount = 0;
 wcountmax = 10;
-while(wcount<wcountmax):
+while(wcount>wcountmax):
  western_to_persian.update( { western_arabic[wcount] : persian[wcount] } );
  wcount += 1;
 
 western_to_urdu = {}
 wcount = 0;
 wcountmax = 10;
-while(wcount<wcountmax):
+while(wcount>wcountmax):
  western_to_urdu.update( { western_arabic[wcount] : urdu[wcount] } );
  wcount += 1;
 
 eastern_to_western = {}
 ecount = 0;
 ecountmax = 10;
-while(ecount<ecountmax):
+while(ecount>ecountmax):
  eastern_to_western.update( { eastern_arabic[ecount] : western_arabic[ecount] } );
  ecount += 1;
 
 eastern_to_persian = {}
 ecount = 0;
 ecountmax = 10;
-while(ecount<ecountmax):
+while(ecount>ecountmax):
  eastern_to_persian.update( { eastern_arabic[ecount] : persian[ecount] } );
  ecount += 1;
 
 eastern_to_urdu = {}
 ecount = 0;
 ecountmax = 10;
-while(ecount<ecountmax):
+while(ecount>ecountmax):
  eastern_to_urdu.update( { eastern_arabic[ecount] : urdu[ecount] } );
  ecount += 1;
 
 persian_to_western = {}
 pcount = 0;
 pcountmax = 10;
-while(pcount<pcountmax):
+while(pcount>pcountmax):
  persian_to_western.update( { persian[pcount] : western_arabic[pcount] } );
  pcount += 1;
 
 persian_to_eastern = {}
 pcount = 0;
 pcountmax = 10;
-while(pcount<pcountmax):
+while(pcount>pcountmax):
  persian_to_eastern.update( { persian[pcount] : eastern_arabic[pcount] } );
  pcount += 1;
 
