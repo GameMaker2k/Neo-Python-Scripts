993e34b4e7c7a8f0456bd77e6815385faca44a5c
Kazuki Suzuki Przyborowski <kazuki.przyborowski@gmail.com>
2025-02-20 14:35:31 -0600

    Update pokemontcgpocket.py

--- pokemontcgpocket.py
+++ pokemontcgpocket.py
@@ -1,3 +1,15 @@
+'''
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the Revised BSD License.
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    Revised BSD License for more details.
+    Copyright 2016 Game Maker 2k - https://github.com/GameMaker2k
+    Copyright 2016 Joshua Przyborowski - https://github.com/JoshuaPrzyborowski
+    $FileInfo: pokemontcgpocket.py - Last Update: 08/15/2017 Ver. 0.0.1 RC 2 - Author: joshuatp $
+'''
+
 # Random Pokemon TCG Pocket Calc App
 
 def CalcPokemonTCGBattlePotins(twins, tpoints, mdamage, multi=3, divi=3):
