23cadec01cfc319d2487fe8f715e0d2f8d3c05d0
Kazuki Suzuki Przyborowski <kazuki.przyborowski@gmail.com>
2025-02-20 14:36:58 -0600

    Update pokemontcgpocket.py

--- pokemontcgpocket.py
+++ pokemontcgpocket.py
@@ -16,3 +16,4 @@ def CalcPokemonTCGBattlePotins(twins, tpoints, mdamage, multi=3, divi=3):
     calcfist = (twins * multi) -  tpoints
     calcsecond = (tpoints / divi) - twins
     calcfinal = (calcfist + calcsecond) + mdamage
+    return calcfinal
